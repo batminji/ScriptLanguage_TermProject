e5e4709734692a8b900ebcb18613671a274d74d9
aecong <dkdus78655@gmail.com>
2024-05-21 23:09:25 +0900

    리스트 박스와 스크롤 바 추가

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class MainGUI:
 
         # 로고 삽입
         original_image = Image.open("logo.png")  # 이미지 로드
-        resized_image  = original_image.resize((50, 50))
+        resized_image = original_image.resize((50, 50))
         img = ImageTk.PhotoImage(resized_image)
         self.canvas.create_image(50, 50, image=img, anchor="center")
 
@@ -30,6 +30,22 @@ class MainGUI:
         # 검색 버튼
         search_button = tk.Button(search_frame, text="검색", command=self.search)
         search_button.pack(side="left", padx=5)
+
+        # 리스트 박스 프레임 생성
+        list_frame = tk.Frame(self.root)
+        self.canvas.create_window(300, 625, width=550, height=275, window=list_frame, anchor="center")
+
+        # 리스트 박스 생성
+        self.salon_list = tk.Listbox(list_frame, width=60, height=30)
+        self.salon_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # 스크롤 바 생성
+        scrollbar = tk.Scrollbar(list_frame)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # 스크롤 바 와 목록 연결
+        self.salon_list.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.salon_list.yview)
 
         self.root.mainloop()
 
