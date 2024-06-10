a85d05280f572b5293341a37a3824d307991a783
aecong <dkdus78655@gmail.com>
2024-06-10 13:40:08 +0900

    spam

--- temp_project.py
+++ temp_project.py
@@ -15,6 +15,7 @@ from tkinter import simpledialog
 
 import telepot
 from tkinter import simpledialog
+import spam
 
 class MainGUI:
     def __init__(self):
@@ -222,7 +223,6 @@ class MainGUI:
                 self.directions_button.pack(side=tk.RIGHT, expand=True)
 
     def update_salon_map(self, salon_data):
-        gmaps = Client(key=self.Google_API_Key)
         transformer = Transformer.from_crs('epsg:2097', 'epsg:4326')
         if salon_data['lat'] and salon_data['lng']:
             x, y = float(salon_data['lat']), float(salon_data['lng'])
